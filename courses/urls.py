6ebaea7714c292208fa6f72151fb137d71c1a328
Dominic Parducci <daparducci@gmail.com>
2020-04-09 14:15:38 -0700

    first commit - added courses templates, views, and models

---
+++ courses/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.course_list),
+    path('<int:course_pk>/<int:step_pk>', views.step_detail),
+    path('<int:pk>/', views.course_detail),
+]
